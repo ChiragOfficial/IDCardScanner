6798d788e46036d402a5426223ca95f21fc5c7cd
Puneet Saravanan Lamamuri <80655583+PuneetSaravanan@users.noreply.github.com>
2023-04-14 15:09:47 +0000

    Co-authored-by: 20BAC10002 <chirag.mohanty2020@vitbhopal.ac.in>

--- rename_dataset.py
+++ rename_dataset.py
@@ -1,10 +1,16 @@
 import os
 
-dataset_list = os.listdir("dataset")
-count = len(dataset_list)
+# dataset_list = os.listdir("dataset")
+# count = len(dataset_list)
 
-for i in range(len(dataset_list)):
-    source = "dataset/"+dataset_list[i]
-    destination = "dataset/"+str(i+count)+".jpg"
-    if(destination not in dataset_list):
-        os.rename(source, destination)
+# for i in range(len(dataset_list)):
+#     source = "dataset/"+dataset_list[i]
+#     destination = "dataset/"+str(i+count)+".jpg"
+#     if(destination not in dataset_list):
+#         os.rename(source, destination)
+
+dataset_path = "/Dataset_annotated"
+for i in range(0, 223):
+    previous_img_path = (str)i+".xml"+".txt"
+    new_img_path = (str)i+".txt"
+    os.rename(previous_img_path, new_img_path)
